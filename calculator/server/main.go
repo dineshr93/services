b14608ae38ee9c856d2547f2dbfc2e7f248e62a1
Dinesh Ravi <dineshr93@gmail.com>
2022-09-05 14:49:26 +0200

    Update main.go

--- calculator/server/main.go
+++ calculator/server/main.go
@@ -7,33 +7,45 @@ import (
 	"os"
 
 	pb "github.com/dineshr93/services/calculator/proto"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-var addr string = "0.0.0.0:50051"
+var addr string = "localhost:50051"
 
 type Server struct {
 	pb.CalculatorServiceServer
 }
 
 func main() {
+
 	if len(os.Args) > 1 {
 		addr = os.Args[1]
 		// log.Println(number)
 
 		// do something with command
 	} else {
-		fmt.Println("Please provide one number as an argument")
-		os.Exit(1)
+		fmt.Println("Please provide address as an argument")
+		// os.Exit(1)
 	}
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v\n", err)
 	}
 	log.Printf("Listening at %s\n", addr)
+	opts := []grpc.ServerOption{}
+	tsl := true
+	if tsl {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed loading certificates: %v\n", err)
+		}
+		opts = append(opts, grpc.Creds(creds))
 
-	s := grpc.NewServer()
+	}
+	s := grpc.NewServer(opts...)
 	pb.RegisterCalculatorServiceServer(s, &Server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v\n", err)
